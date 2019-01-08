3bbb8ceab319a7c648af262293546eea89f4ae84
Lizband <labrys.git@gmail.com>
2019-01-08 18:49:31 -0500

    Add up down movement

--- tut.py
+++ tut.py
@@ -33,6 +33,7 @@ x = (display_width * 0.45)
 y = (display_height * 0.8)
 
 x_change = 0
+y_change = 0
 
 
 # Game loop
@@ -44,6 +45,7 @@ while not dead:
         if event.type == pygame.QUIT:
             dead = True
 
+# Left right movement
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x_change = -2
@@ -55,7 +57,21 @@ while not dead:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_change = 0
 
+# Up Down movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                y_change = -2
+            elif event.key == pygame.K_DOWN:
+                y_change = 2
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_change = 0
+
+
+
     x += x_change
+    y += y_change
 
     game_display.fill(black)
     char(x, y)
