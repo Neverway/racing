f9448d7c20f459d00e9f1fb809fed42d6f6032b8
Lizband <labrys.git@gmail.com>
2019-01-08 17:30:31 -0500

    Add display window with title

---
+++ tut.py
@@ -0,0 +1,10 @@
+import pygame
+
+#This is important and needs to be first
+pygame.init()
+
+#Set the game windows resolution
+gameDisplay = pygame.display.set_mode((800,600))
+
+#Set windows name
+pygame.display.set_caption('Car Movement Test')
