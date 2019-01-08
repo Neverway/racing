225c5161b84c33d4317949ef04ac30b74f50f813
Lizband <labrys.git@gmail.com>
2019-01-08 17:38:14 -0500

    Made window constant and track inputs

--- tut.py
+++ tut.py
@@ -3,8 +3,22 @@ import pygame
 #This is important and needs to be first
 pygame.init()
 
-#Set the game windows resolution
+#Windows resolution
 gameDisplay = pygame.display.set_mode((800,600))
 
-#Set windows name
+#Windows name
 pygame.display.set_caption('Car Movement Test')
+
+#Game clock(FPS)
+clock = pygame.time.Clock()
+
+#Game loop
+dead = False
+
+while not dead:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            dead = True
+
+        print(event)
